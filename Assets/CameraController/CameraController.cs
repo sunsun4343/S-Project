42c05e00c5d3bf4f77b76e6625075608150449c3
ss.lee <ss.lee@morph.kr>
2018-10-23 14:16:34 +0900

    camera

---
+++ Assets/CameraController/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    Camera camera;
+
+    private void Awake()
+    {
+        camera = this.GetComponent<Camera>();
+
+
+    }
+
+    private void Update()
+    {
+
+
+        
+    }
+
+}
